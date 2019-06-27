e7b520377aea208b0d895726ab70033e798fcc6d
vinirg22 <aramburo.irving@gmail.com>
2019-06-27 14:31:44 -0700

    Set up complete for express assignment and folders and files created

---
+++ FriendFinder/server.js
@@ -0,0 +1,30 @@
+
+const express = require("express");
+const path = require("path");
+
+
+
+// Tells node that we are creating an "express" server
+const app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// ROUTER
+
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+
+
+// The below code effectively "starts" our server
+app.listen(PORT, () => {
+  console.log("App listening on PORT: " + PORT);
+});
