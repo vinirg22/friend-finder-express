f6746c7461a15fdbf7861e173195bf9609cf8826
vinirg22 <aramburo.irving@gmail.com>
2019-06-28 09:57:39 -0700

    Updated htmlRoutes.js for app.get in the index.home file from being /home to just /. This was done for deployment purposes

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 module.exports = (app) => {
 
-    app.get("/home", (req, res) => {
+    app.get("/", (req, res) => {
       res.sendFile(path.join(__dirname, "../public/home.html"));
     });
   
