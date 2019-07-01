a20d6493e9038544e4e5782b9fb36b92578a2ebe
vinirg22 <aramburo.irving@gmail.com>
2019-06-30 21:30:45 -0700

    Worked on routing routes, html and api js to populate results when survey has been taken. Server js file linked to other files for results

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -10,10 +10,71 @@ module.exports = (app) => {
     });
 
     app.post("/api/friends", (req, res) => {
+        //Comparing user with their best friend match 
+        const difference = 0;
+        
+        const bestMatch = {
+            name: "",
+            photo: "",
+            friendDiff: 1000
+        };
 
-        if (friends.length) {
-            friends.push(req.body);
-            res.json(true);
+      // result of user's survey
+        const userData = req.body;
+        const userName = userData.name;
+        const userScores = userData.scores;
+        
+        const b = userScores.map(function (item) {
+            return parseInt(item, 10);
+        });
+        userData = {
+            "name": req.body.name,
+            "photo": req.body.photo,
+            "scores": b
         }
+
+
+        console.log("Name: " + userName);
+        console.log("User Score " + userScores);
+
+        const sum = b.reduce((a, b) => a + b, 0);
+        console.log("Sum of users score " + sum);
+        console.log("Best match friend diff " + bestMatch.friendDiff);
+
+
+        console.log("+++++++=================++++++++++");
+        // Loop through all the friend chances
+        for (const i = 0; i < friends.length; i++) {
+
+            console.log(friends[i].name);
+            difference = 0;
+            console.log("Total Diff " + difference);
+            console.log("Best match friend diff " + bestMatch.friendDiff);
+
+            const friendScore = friends[i].scores.reduce((a, b) => a + b, 0);
+            console.log("Total friend score " + friendScore);
+            difference += Math.abs(sum - friendScore);
+            console.log(" -------------------> " + difference);
+         
+            if (difference <= bestMatch.friendDiff) {
+
+            
+                bestMatch.name = friends[i].name;
+                bestMatch.photo = friends[i].photo;
+                bestMatch.friendDiff = difference;
+            
+
+            }
+            console.log(difference + " Total Difference");
+
+        }
+       
+        friends.push(userData);
+        console.log("New User added");
+      
+        // Return a JSON with the user's bestMatch.
+        res.json(bestMatch);
+
     });
+
 }
